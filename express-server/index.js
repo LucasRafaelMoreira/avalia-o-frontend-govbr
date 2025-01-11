e2f02f3ca2d5e7cbbe21e93764dd308e667b30a3
LucasRafaelMoreira <lucasrafaelmoreira06@gmail.com>
2025-01-11 20:46:01 -0300

    comitando as mudanças realizadas

---
+++ express-server/index.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const ejs = require('ejs');
+const app = express();
+const path = require('path');
+const port = 3000;
+
+// app.get('/', (req, res) => {
+//   res.send('Hello, World!'
+//   );
+// });
+
+app.set('view engine','ejs'); 
+app.use(express.static(path.join(__dirname, 'public')));
+
+// app.engine('ejs', require('ejs').__express);
+
+app.get('/', (req, res) => {res.render('index')});
+app.get('/cadastroPessoa', (req, res) => {res.render('cadastroPessoa')});
+app.get('/cadastroImovel', (req, res) => {res.render('cadastroImovel')});
+app.get('/consultaPessoa', (req, res) => {res.render('consultaPessoa')});
+app.get('/consultaImovel', (req, res) => {res.render('consultaImovel')});
+app.get('/iptu', (req, res) => {res.render('iptu')});
+
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}/`);
+});
